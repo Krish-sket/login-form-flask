1f6c96d6187f46909d7906f729cdb520249a461e
Krish-sket <theworldofgamingksmvd@gmail.com>
2024-11-19 20:37:47 +0530

    new

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from werkzeug.security import generate_password_hash,check_password_hash
 import sqlite3
 
 app=Flask(__name__)
-app.config['SECRET KEY']='e274466e74fe3648251eca04cd9bc910'
+app.config['SECRET_KEY']='e274466e74fe3648251eca04cd9bc910'
 
 def init_db():
-    conn=sqlite3.connect('user_db.db')
+    conn=sqlite3.connect('user_data.db')
     cursor=conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS users(
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,10 +28,10 @@ def login():
         username=request.form['username']
         password=request.form['password']
 
-        conn=sqlite3.connect('user_db.db')
+        conn=sqlite3.connect('user_data.db')
         cursor=conn.cursor()
         cursor.execute("SELECT * FROM users WHERE username=?",(username,))
-        user=cursor.fetchhone()
+        user=cursor.fetchone()
         conn.close()
 
         if user and check_password_hash(user[2],password):
@@ -50,10 +50,10 @@ def register():
         password=request.form['password']
         email=request.form['email']
 
-        hashed_password=generate_password_hash(password,method='')
+        hashed_password=generate_password_hash(password,method='pbkdf2:sha256')
 
         try:
-            conn=sqlite3.connect('user_db.db')
+            conn=sqlite3.connect('user_data.db')
             cursor=conn.cursor()
             cursor.execute('INSERT INTO users(username,password,email)VALUES (?,?,?)',(username,hashed_password,email))
             conn.commit()
@@ -63,21 +63,21 @@ def register():
         except sqlite3.IntegrityError:
             flash('Username or email already exists','danger')
 
-        return render_template('registration.html')
+    return render_template('registration.html')
 
-    @app.route('/Welcome')
-    def welcome():
-        if 'username' in session:
-            return render_template('welcome.html',name=session['username'],msg='Login Successful')
-        else:
-            flash('Please log in to access this page', 'danger')
-            return redirect(url_for('login'))
-
-    @app.route('/logout')
-    def logout():
-        session.pop('username',None)
-        flash('You have been logged out','info')
+@app.route('/welcome')
+def welcome():
+    if 'username' in session:
+        return render_template('welcome.html',name=session['username'],msg='Login Successful')
+    else:
+        flash('Please log in to access this page', 'danger')
         return redirect(url_for('login'))
 
-    if __name__ =='__main__':
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    flash('You have been logged out','info')
+    return redirect(url_for('login'))
+
+if __name__ =='__main__':
         app.run(debug=True)
